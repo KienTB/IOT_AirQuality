51d2170371e5ecfea3749b9098094402e1ca46c0
Kien <kientrung14122003@gmail.com>
2025-04-28 17:04:53 +0700

    done

--- AirGuard/src/main/java/com/example/iot/AirGuard/Config/DialogflowConfig.java
+++ AirGuard/src/main/java/com/example/iot/AirGuard/Config/DialogflowConfig.java
@@ -1,5 +1,6 @@
 package com.example.iot.AirGuard.Config;
 
+import com.google.api.client.util.Value;
 import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +11,13 @@ import java.util.Collections;
 
 @Configuration
 public class DialogflowConfig {
+    @Value("${dialogflow.credentials.path}")
+    private String credentialsPath;
 
     @Bean
     public GoogleCredentials dialogflowCredentials() throws IOException {
-        // Đọc file JSON key từ thư mục resources
         return GoogleCredentials.fromStream(
-                        new FileInputStream("E:\\Dowloads\\Dowloads\\IOT\\AirGuard\\AirGuard\\src\\main\\resources\\dialogflow-credentials.json"))
+                        new FileInputStream("E:\\Dowloads\\IOT\\AirGuard\\AirGuard\\src\\main\\resources\\dialogflow-credentials.json"))
                 .createScoped(Collections.singleton("https://www.googleapis.com/auth/cloud-platform"));
     }
 }
